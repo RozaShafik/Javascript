79268120342f2992ac282319d2fc29d1bd9cd5c1
RozaShafik <45145174+RozaShafik@users.noreply.github.com>
2019-01-25 13:43:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+//step1:statement saying "Hello World!" in deferent languages
+console.log("Hello World!");
+console.log("Halo, dunia!");
+console.log("Ciao, mondo!");
+console.log("Hola, mundo!");
+//step2:Find a solution for this error
+console.log("I'm awesome");
+//step3
+let newX;
+console.log("the value of my variable newx will be:");
+newX=83;
+console.log(newX);
+//step4
+let newY;
+newY="Aug"
+console.log("The value of my string is",newY);
+//step5:round the number 7.25
+let x=8;
+console.log("the value of x will be:",x);
+let y=5;
+console.log("the value of my string will be:",y);
+let z=7.25;
+let a = Math.round(z);
+console.log("the value of a:",a);
+let r;
+    if (a < z) {
+        r=z;
+    }
+
+    else {
+        r=a;
+    } 
+
+    for (i=0;i<3;i++){
+      let MyfavSport =  ["swmming","tennis","basketball"];
+        console.log("may favorite sport are:",MyfavSport[i]);
+    }
+      for (j=0;j<3;j++){
+        let MyfavAnimals =["cat","dog","birds"];
+        console.log("my favorite animals are:",MyfavAnimals[j]);
+      }
+// step6: array
+let emArr=[];
+console.log("here is an empty array",emArr);
+//favanimals
+let myFavoritanimals=["dolphin","horse","cat"];
+console.log(myFavoritanimals);
+let danasfavanimals=["baby pig"];
+let newArray= myFavoritanimals+"," +danasfavanimals;
+console.log("the new array",newArray);
+//step7:more strings
+let myString = "this is a test";
+console.log("print out the length of my string:",myString.length);
+//step8
+    let foo = 3;
+    let newfoo="foo";
+    console.log("The value of my variable foo is: " + foo);
+    console.log("The value of my variable newfoo is: " + newfoo);
+    if (typeof(foo)===typeof(newfoo)) {
+        console.log(" the same");
+    }
+    else{
+        console.log("not the same");
+    }
+   //step9
+   let x1= 7;
+   let y1=x1 % 3;
+   console.log( "the new value of x =",y1);
+   let multi=[83,"aug",8];
+   console.log(6/0);
+   console.log(10/0);
+    if(6/0===10/0){
+        console.log("the same");   
+    }
+    else{
+        console.log("not the same");
+    }
+
+    
